145f3bb1c24edaf1d6b5638901795b9e26ea4c39
rupalijain1518 <39921763+rupalijain1518@users.noreply.github.com>
2020-08-24 23:39:18 +0530

    fix ui

--- src/components/homeElements/displayNews.js
+++ src/components/homeElements/displayNews.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+
+
 class NewsDetail extends Component{
   constructor(props) {
     super(props);
@@ -33,30 +35,39 @@ this.unsubscribe = this.ref.orderBy("heading", "asc").onSnapshot(this.onCollecti
 render(){
 
   return (
-    
-    <div className = "container">
-    <br/>
-
-      <label htmlFor="formGroupExampleInput">News Update</label>
-  
-    { this.state.filtered.map(pack =>
-      
-  <div className="card text-center" key={pack.key} styles="width: 18rem;">
-      <br/>
-<center>  {pack.url ? <img src={pack.url} alt="..." className="rounded" height="170px" width="170px"/> : "No picture"}
-     </center>
-  <div className="card-body">
-  <h5 className="card-title">Heading:{pack.heading} </h5>
-  <p className="card-text"><a href={pack.link}><small className="text-muted">News Link:{pack.link}</small></a></p>
-  </div>
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item">Description:{pack.description}</li>
-    <li className="list-group-item">Date:{pack.date}</li>
-  </ul>
+   <div >
+ <center>
+ <label  htmlFor="formGroupExampleInput">Daily News</label>
+</center>
 <br/>
-  </div>
-  )}  
-  </div>
+<div className="row">
+
+		<div className="offset-md-4 col-md-4 col-sm-12 SetsDiscDatas">
+
+    <div className="row SetNews_portal">
+					<div className="col-sm-12">
+						{this.state.filtered.map(pack =>
+            <div className="row">
+							<div className="col-sm-12 Set_news_portal">
+								<a href={pack.url} target="_blank">
+									<img src={pack.url} alt="No Preview Available" />
+								</a>
+							    <div className="card-body">
+            <h4 className="card-title Card_Title">{pack.heading}</h4>
+								    <p className="SetDateYear">
+								    	<i className="fa fa-calendar-o" aria-hidden="true"></i>
+            &nbsp;<span id="date">{pack.date}</span>
+								    </p>
+            <a className="Set_WebLink" href={pack.link} target="_blank">{pack.link}</a>
+            <p className="card-text Card_Text">{pack.description}</p>
+								 </div>						
+							</div>
+					</div>)}
+					</div>
+				</div>
+        </div></div>
+        </div>
+   
  );
 }
 }
